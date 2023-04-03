fbe63facb1da1f0e3906e1012d9861509acbf079
Prakhar Sharma <1915438@swansea.ac.uk>
2023-04-03 16:49:58 +0100

    a basic tabbed interface added

---
+++ main_tabs.py
@@ -0,0 +1,29 @@
+    # # Add things to the empty screen
+    # def add_to_MyApp(self):   
+    #     # Create tabs to the Qwidget
+    #     self.tab1.layout = QVBoxLayout(self)
+    #     self.l = QLabel()
+    #     self.l.setText("This is the first tab")
+    #     self.tab1.layout.addWidget(self.l)
+    #     self.tab1.setLayout(self.tab1.layout)
+
+    # # Initialize main tab screen
+    # def main_tabs(self):
+    #     # Add any tabs that you want display in the main screen.
+    #     tab = QTabWidget(self)
+
+    #     tab1 = QWidget(self)
+    #     tab.addTab(tab1, "Geometry")
+
+    #     tab2 = QWidget(self)
+    #     tab.addTab(tab2, "Constraints")
+
+    #     tab3 = QWidget(self)
+    #     tab.addTab(tab3, "Compile")
+
+    #     tab4 = QWidget(self)
+    #     tab.addTab(tab4, "Training")
+
+
+    #     tab5 = QWidget(self)
+    #     tab.addTab(tab5, "Results")
