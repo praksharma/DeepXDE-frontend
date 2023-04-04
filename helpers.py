3ce94ec8317767d73c7c3060ef70db21eb0e523c
Prakhar Sharma <1915438@swansea.ac.uk>
2023-04-04 09:38:28 +0100

    Added config.py in helpers.py

---
+++ helpers.py
@@ -0,0 +1,31 @@
+from PyQt5.QtWidgets import QLabel
+import configparser
+
+
+def gen_conf_file():
+    """
+    The code creates an ini file to store confgurations.
+    """
+    config = configparser.ConfigParser()
+    config['DEFAULT'] = {'workdir': '$HOME/DeepXDE_simulations/'}
+
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
+
+def empty_label(self, row , col, rowSpan, colSpan, height):
+    """
+    Add an empty label to put suitable gaps in the grid layout.
+
+    Args:
+        row (int): row number
+        col (int): column number
+        rowSpan (int): number of row span
+        colSpan (int): number of col span
+        height (int): height of the QLabel
+    """
+    # Add an empty QLabel with suitable height to increase the gap
+    self.empty_label = QLabel()
+    self.empty_label.setFixedHeight(height) # set the height of the empty label as per your requirement
+    self.grid.addWidget(self.empty_label, row, col, rowSpan, colSpan)
+
